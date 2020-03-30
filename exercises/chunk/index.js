cc7bece8a49576c5e7eca0e84964b6d2d17f0a69
bryan leighton <bryan.leigh77@gmail.com>
2020-03-29 18:54:04 -0700

    chunk solution #1

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,29 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+}
 
+// function chunk(array, size) {
+//    // declare new arr chunked
+//     // iterate over array (each el in unchunked arr(array))
+//       // retrieve last el in chunked arr
+//       // if el does NOT exist OR length == size
+//         // push new chunk into "chunked" arr
+//       // else add to current chunked
+//     // look at last el inside chunked arr
+//     const chunked = [];
+//     for (el of array) {
+//       // if no sub array of the length of subarray is size.. ADD a new chunk.
+//       const last = chunked[chunked.length -1];
+//       if (!last || last.length === size) {
+//         chunked.push([el]); // push new CHUNK with el inside!
+//       } else {
+//         // else if sub array is not full (last) .. add el to it
+//         last.push(el);
+//       }
+//     }
+//     return chunked;
+// }
+chunk([1,2,3,4], 2);
 module.exports = chunk;
