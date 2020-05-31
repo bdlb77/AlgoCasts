0ced1acb79639fa05867c48d36f77e3f2be5cc8a
bryan leighton <bryan.leigh77@gmail.com>
2020-05-31 16:00:36 -0700

    fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,35 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+//   const res = [0, 1];
+  
+//   sum = 0;
+//   for (let i = 2; i <= n; i++) {
+//     res.push((res[i - 1] + res[i - 2]));
+//   }
+//   return res[n]
+// }
+function memoize(fn) {
+  const cache = {};
+  // return dramatically faster fn
+  return function(...args){
+    if (cache[args]) return cache[args];
+
+    const result = fn.apply(this, args)
+    cache[args] = result;
+
+    return result;
+  }
+}
+function fib(n) {
+  // if (n === 0) return 0;
+  // if (n == 1) return 1;
+  if (n < 2) return n;
+
+  return (fib(n - 1)) + fib(n - 2);
+}
+
+fib = memoize(fib);
 
 module.exports = fib;
